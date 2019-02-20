2caf887a0c4cf689972e99f6e4c5ae24a60d730d
Benjamin Hallquist <benhall84707@gmail.com>
2019-02-20 11:19:04 -0500

    avatar attribute added

--- character.py
+++ character.py
@@ -6,7 +6,8 @@
     
 class Character():
     # in python - "dunder init" is the method used for how you define a constructor
-    def __init__(self, new_name):
+    def __init__(self, new_name, new_avatar):
         # 'self' is the customary way to refer to the instance being built
         # in some other languages, they use 'this'
         self.name = new_name
+        self.avatar = new_avatar
