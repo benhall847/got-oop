6824e4cc23f73daf2521d6b7a121f92ab4abe0dd
Benjamin Hallquist <benhall84707@gmail.com>
2019-02-20 11:53:08 -0500

    modified greet method to greet another character

--- character.py
+++ character.py
@@ -12,5 +12,5 @@ class Character():
         self.name = new_name
         self.avatar = new_avatar
         self.inventory = []
-    def greet(self):
-        return "Hello, I am %s!" % self.name
+    def greet(self, person):
+        return "Hello %s, I am %s!" % (person.name, self.name)
