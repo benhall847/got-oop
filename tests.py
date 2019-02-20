c847c177b34128c9b1a7e7a8817c1fc0f008270f
Benjamin Hallquist <benhall84707@gmail.com>
2019-02-20 11:21:43 -0500

    added a test file

---
+++ tests.py
@@ -0,0 +1,10 @@
+# these arent real tests
+from character import Character
+
+# Characters can be instantiated with name and avatar
+
+ayra = Character("Ayra Stark", "PHOTO.png")
+jon = Character("Jon Snow", "SUPERPHOTO.png")
+print(jon.name, jon.avatar, ayra.name, ayra.avatar)
+
+
